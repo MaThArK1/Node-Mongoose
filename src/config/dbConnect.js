e4e358e3212e3704b86095a236f2fcdd90f9c4c8
Matheus-Barucho <87724736+Matheus-Barucho@users.noreply.github.com>
2022-05-10 08:26:22 -0300

    Update dbConnect.js

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 //mongoose.connect("mongodb://127.0.0.1:27017/alura-node"); caso fosse conexão local
-mongoose.connect("mongodb+srv://matheus_barucho:Matharkdoge25122001@cluster0.tfgfb.mongodb.net/alura-node");
+mongoose.connect("mongodb+srv://matheus_barucho:<<senha>>@cluster0.tfgfb.mongodb.net/alura-node");
 
 let db = mongoose.connection;
 
