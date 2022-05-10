b9c03e0fb333ebe4009bd0c6c8f246e55bdf45e1
MaThArK1 <matheus_barucho@hotmail.com>
2022-05-10 10:56:15 -0300

    Busca por editora e populate

--- src/routes/livrosRoutes.js
+++ src/routes/livrosRoutes.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 router
     .get("/livros", LivroController.listarLivros)
+    .get("/livros/busca", LivroController.listarLivrosPorEditora)
     .get("/livros/:id", LivroController.listarLivroPorId)
     .post("/livros", LivroController.cadastrarLivro)
     .put("/livros/:id", LivroController.atualizarLivro)
